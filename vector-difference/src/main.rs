ecf977b78c933f2d9bf374bfc7028ac69c5f487e
Slava Fumin <slavafumin@gmail.com>
2025-07-03 10:45:39 +0300

    fn<T> add

--- vector-difference/src/main.rs
+++ vector-difference/src/main.rs
@@ -1,12 +1,13 @@
 use std::collections::HashSet;
 
-fn vector_difference(v1: &[i32], v2: &[i32]) -> Vec<i32> {
+fn vector_difference<T>(v1: &[T], v2: &[T]) -> Vec<T>
+where
+    T: Eq + Copy + std::hash::Hash,
+{
     let set: HashSet<_> = v2.into_iter().collect();
-    let mut res: Vec<i32> = vec![];
+    let mut res: Vec<T> = vec![];
     for num in v1 {
-        if set.contains(num) {
-            continue;
-        } else {
+        if !set.contains(num) {
             res.push(*num);
         }
     }
@@ -16,6 +17,7 @@ fn main() {
     let vec1 = vec![1, 2, 5, 8, 7];
     let vec2 = vec![5, 8, 5, 11, 7];
     dbg!(vector_difference(&vec1, &vec2));
+    println!("{vec1:?}{vec2:?}")
 }
 
 #[cfg(test)]
